c4df4cf794cd6c735b0941755941ba42e9b9d01d
Hayato Arai <hayato4d@gmail.com>
2024-07-17 21:37:03 +0900

    Fix eval.py

--- eval.py
+++ eval.py
@@ -3,9 +3,11 @@ import os
 import hydra
 import numpy as np
 import torch
-from library.datasets import ThingsMEGDataset
-from library.models import BasicConvClassifier
-from library.utils import set_seed
+from library.brain_module import BrainModule
+from library.classification_module import MLPHead
+from library.datasets import ThingsMEGDatasetWithImages
+from library.image_module import PRETRAINED_MODEL_TO_LATENT_DIMENSION
+from library.utils import get_model_id, set_seed
 from omegaconf import DictConfig
 from termcolor import cprint
 from tqdm import tqdm
@@ -15,12 +17,17 @@ from tqdm import tqdm
 @hydra.main(version_base=None, config_path="configs", config_name="config")
 def run(args: DictConfig) -> None:
     set_seed(args.seed)
-    savedir = os.path.dirname(args.model_path)
+    model_id = get_model_id(args)
+    logdir = f"outputs/{model_id}"
+
+    latent_dim = PRETRAINED_MODEL_TO_LATENT_DIMENSION[
+        (args.image_module.repo, args.image_module.model)
+    ]
 
     # ------------------
     #    Dataloader
     # ------------------
-    test_set = ThingsMEGDataset("test", args.data_dir)
+    test_set = ThingsMEGDatasetWithImages("test", args.data_dir)
     test_loader = torch.utils.data.DataLoader(
         test_set,
         shuffle=False,
@@ -31,22 +38,30 @@ def run(args: DictConfig) -> None:
     # ------------------
     #       Model
     # ------------------
-    model = BasicConvClassifier(
-        test_set.num_classes, test_set.seq_len, test_set.num_channels
-    ).to(args.device)
-    model.load_state_dict(torch.load(args.model_path, map_location=args.device))
+    classifier = MLPHead(in_dim=latent_dim, num_classes=test_set.num_classes)
+    classifier.load_state_dict(
+        torch.load(os.path.join(logdir, f"classifier_best_{model_id}.pt"))
+    )
+    brain_module = BrainModule(out_dim=latent_dim).to(args.device)
+    brain_module.load_state_dict(
+        torch.load(os.path.join(logdir, f"brain_module_best_{model_id}.pt"))
+    )
 
     # ------------------
     #  Start evaluation
     # ------------------
     preds = []
-    model.eval()
-    for X, subject_idxs in tqdm(test_loader, desc="Validation"):
-        preds.append(model(X.to(args.device)).detach().cpu())
+    classifier.eval()
+    brain_module.eval()
+    for brain_X, subject_idx in tqdm(test_loader, desc="test"):
+        clip_latent_vector, mse_latent_vector = brain_module(
+            brain_X.to(args.device), subject_idx.to(args.device)
+        )
+        preds.append(classifier(clip_latent_vector)).detach().cpu()  # type: ignore
 
     preds = torch.cat(preds, dim=0).numpy()
-    np.save(os.path.join(savedir, "submission"), preds)
-    cprint(f"Submission {preds.shape} saved at {savedir}", "cyan")
+    np.save(os.path.join(logdir, "submission"), preds)
+    cprint(f"Submission {preds.shape} saved at {logdir}", "cyan")  # type: ignore
 
 
 if __name__ == "__main__":
